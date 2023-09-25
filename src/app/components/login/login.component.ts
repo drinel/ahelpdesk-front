633e7e8f48a7598b2f3495a53aaa702034653114
drinel <will.i.am.002@hotmail.com>
2023-09-25 15:23:07 -0300

    autenticação de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credentials';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -16,24 +18,30 @@ export class LoginComponent implements OnInit{
   }
 
   email = new FormControl(null, Validators.email);
-  senha = new FormControl(null, Validators.minLength(8));
+  senha = new FormControl(null, Validators.minLength(3));
 
-   constructor(private toast: ToastrService){}
+   constructor(private toast: ToastrService, private service: AuthService, private router: Router){}
 
   ngOnInit(): void {
     
   }
 
   logar(){
-    this.toast.error("Usuário ou senha inválidos!", 'Login');
-    this.creds.senha = '';
+     this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.successfulLogin(resposta.headers.get('Authorization').substring(7));
+      this.router.navigate(['']);
+      //this.toast.info(resposta.headers.get('Authorization'))
+     }, () => {
+      this.toast.error('Usuário ou senha inválidos!');
+     })
+
+   // this.toast.error("Usuário ou senha inválidos!", 'Login');
+   // this.creds.senha = '';
   }
 
   validarCampos(): boolean {
-    if(this.email.valid && this.senha.valid){
-      return true;
-    }
-      return false;
+    return this.email.valid && this.senha.valid
+    
   }
 
 }
