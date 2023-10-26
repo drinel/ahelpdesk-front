76f288ea113f6734e8cd406214d80ddfa8d8ac3c
drinel <will.i.am.002@hotmail.com>
2023-10-26 04:17:24 -0300

    Rota component chamado-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { ClienteDeleteComponent } from './components/cliente/cliente-delete/clie
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 
 const routes: Routes = [
   {
@@ -33,7 +34,8 @@ const routes: Routes = [
       {path: 'clientes/update/:id', component: ClienteUpdateComponent},  
       {path: 'clientes/delete/:id', component: ClienteDeleteComponent},
 
-      {path: 'chamados', component: ChamadoListComponent}
+      {path: 'chamados', component: ChamadoListComponent},
+      {path: 'chamados/create', component: ChamadoCreateComponent}
     ]
   }
 ];
